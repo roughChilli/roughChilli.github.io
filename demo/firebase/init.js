70074df11e360b2894cc9f93729f9c9abc0f1efe
Zachary qin <zach@MacBookPro.local>
2017-07-17 22:54:22 +1000

    firebase demo init

---
+++ demo/firebase/init.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAH1E9h6pPxQy2KWC8ITD5LktfwPoewoK0",
+    authDomain: "welcome-first.firebaseapp.com",
+    databaseURL: "https://welcome-first.firebaseio.com",
+    projectId: "welcome-first",
+    storageBucket: "welcome-first.appspot.com",
+    messagingSenderId: "388442526684"
+  };
+  firebase.initializeApp(config);
+</script>
